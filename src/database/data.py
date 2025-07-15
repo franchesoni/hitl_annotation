63ece07adc7dea77343436d6dc171fd52a826ea7
franchesoni <marchesoniacland@gmail.com>
2025-07-15 09:48:25 +0200

    draft description

---
+++ src/database/data.py
@@ -0,0 +1,6 @@
+
+# Database connection and query functions
+# the database is composed of three tables, samples, annotations and predictions
+# samples contains the images
+# annotations contains the image classes, bboxes (with class) and points (with class) and the associations to the images
+# predictions contains (for the cls case) the probabilities for each class
